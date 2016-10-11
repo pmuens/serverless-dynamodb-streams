18c517847dcab209b3ae8c28b7055a4b55d5354c
Philipp Muens <raute1337@gmx.de>
2016-10-11 10:13:15 -0700

    Create handler.js

---
+++ handler.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+module.exports.updateProfile = (event, context, callback) => {
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+  const data = event;
+  data.updatedAt = new Date().getTime();
+
+  const params = {
+    TableName: 'users',
+    Item: data
+  };
+
+  return dynamoDb.put(params, (error, data) => {
+    if (error) {
+      callback(error);
+    } else {
+      callback(null, { message: 'Profile successfully updated', params });
+    }
+  });
+};
+
+module.exports.logger = (event, context, callback) => {
+  // print out the event information on the console (so that we can see it in the CloudWatch logs)
+  console.log(`The following happend in the Database:\n${JSON.stringify(event.Records[0].dynamodb, null, 2)}`);
+
+  callback(null, { event });
+};
