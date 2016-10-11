2bca8e96393de516f2398c59604c055abcfdf083
Philipp Muens <raute1337@gmx.de>
2016-10-11 11:19:32 -0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -24,7 +24,7 @@ module.exports.updateProfile = (event, context, callback) => {
 
 module.exports.logger = (event, context, callback) => {
   // print out the event information on the console (so that we can see it in the CloudWatch logs)
-  console.log(`The following happend in the Database:\n${JSON.stringify(event.Records[0].dynamodb, null, 2)}`);
+  console.log(`The following happend in the DynamoDB database table "users":\n${JSON.stringify(event.Records[0].dynamodb, null, 2)}`);
 
   callback(null, { event });
 };
